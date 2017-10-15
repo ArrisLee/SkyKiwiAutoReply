8aac5186bf5d4b610c1a3f936eb83f9c5cc7fd2c
ArrisLee <arris_li@hotmail.com>
2017-10-16 11:28:04 +1300

    remove document ready, extend default pause

--- myscript.js
+++ myscript.js
@@ -1,20 +1,19 @@
-//start process when the page is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Process start...");
-  //initial reply message(s)
-  var dataset = ["your messages one","your messages two"];
+console.log("Process start...");
+//initial reply message(s)
+var dataset = ["your messages one","your messages two"];
 
-  //set minumun and maxiam waiting time for autoreply
-  var max = 180000;
-  var min = 100000;
-  var pause = Math.random() * (max - min) + min;
+//set minumun and maxiam waiting time for autoreply
+var max = 300000;
+var min = 120000;
+var pause = Math.random() * (max - min) + min;
 
-  //randomly selected a reply message from dataset
-  var index = Math.floor(Math.random() * dataset.length);
-  var msg = dataset[index];
-  console.log("msg: |"+ msg + "| will be posted after " + (pause/1000).toFixed(1) + " seconds");
+//randomly selected a reply message from dataset
+var index = Math.floor(Math.random() * dataset.length);
+var msg = dataset[index];
+console.log("msg: |"+ msg + "| will be posted after " + (pause/1000).toFixed(1) + " seconds");
 
-  //auto answer verify question
+//auto answer verify question
+setTimeout(function(){
   var x = document.getElementsByClassName('p_pop p_opt');
   if (x.length != 0) {
     var question = x[0].textContent.substr(9,7);
@@ -24,10 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
   } else {
     console.log('No question identified.');
   }
+}, 3500);
 
-  //set time out and submit the reply
-  setTimeout(function(){
-    document.getElementById('fastpostmessage').value = msg;
-    document.getElementById("fastpostform").submit();
-  }, pause);
-}, false);
+//set time out and submit the reply
+setTimeout(function(){
+  document.getElementById('fastpostmessage').value = msg;
+  document.getElementById("fastpostform").submit();
+}, pause);
